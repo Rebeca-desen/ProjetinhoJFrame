fe15641e7285140c100cbfe7d938091e56f6ed28
Rebeca-desen <rebecarss01@gmail.com>
2021-03-30 09:46:35 -0300

    adicionando cadastro de ingredientes- erro por falta de main

--- src/help/acoes.java
+++ src/help/acoes.java
@@ -16,13 +16,29 @@ public class acoes {
   float[][] estoque= new float[6][3];
     String[] ingredientes= new String[6];
     String input_estoque;
-    String ingredientes_input;
+    String ingredientes_input = null;
     
     for(int i=0; i < ingredientes.length;  i++){ 
-    
-    ingredientes_input= JOptionPane.showInputDialog(null, "Adicione o nome do " + (i+1) + "° " + "aluno(a):", null, JOptionPane.QUESTION_MESSAGE);
+    for(int iEstoque=0; iEstoque < estoque[i].length; iEstoque++){
+    ingredientes_input= JOptionPane.showInputDialog(null, "Adicione o nome do ingrediente:");
     ingredientes[i]= ingredientes_input;
     
+    input_estoque=JOptionPane.showInputDialog(null, "Adicione a quantidade de " + ingredientes[i] + " em estoque");
+    estoque[i][iEstoque]= Float.valueOf(input_estoque);
+    
+    input_estoque=JOptionPane.showInputDialog(null, "Adicione a quantidade mínima que " + ingredientes[i] + " precisa ter em estoque");
+    estoque[i][iEstoque]= Float.valueOf(input_estoque);
+    
+     input_estoque=JOptionPane.showInputDialog(null, "Adicione o valor unitário de " + ingredientes[i]);
+     estoque[i][iEstoque]= Float.valueOf(input_estoque);
+      
     }
-}
+    
+    }
+ for(int i=0; i < ingredientes.length;  i++){ 
+    for(int iEstoque=0; iEstoque < estoque[i].length; iEstoque++){
+    JOptionPane.showMessageDialog(null, "Produto: " + ingredientes[i] + "\n "  +  "informações  \n" + estoque[i][iEstoque]);
+    }
+ }
+    }
 }
